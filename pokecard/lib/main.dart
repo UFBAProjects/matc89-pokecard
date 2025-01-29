ca40efdb54ae9ff7931727891b4c8d0bb91d242a
Larissa Hora <larissa.horaa@gmail.com>
2025-01-28 22:07:52 -0300

    add bottom nav

--- pokecard/lib/main.dart
+++ pokecard/lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:pokecard/mistery_packet/mistery_packet_list_page.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'package:pokecard/deck/pages/deck_list_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -10,5 +11,64 @@ void main() async {
     options: DefaultFirebaseOptions.currentPlatform,
   );
   runApp(
-      const ProviderScope(child: MaterialApp(home: MisteryPacketListPage())));
+    const ProviderScope(
+      child: MaterialApp(
+        home: HomePage(),
+      ),
+    ),
+  );
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+
+  final List<Widget> _pages = <Widget>[
+    const MisteryPacketListPage(),
+    const Placeholder(),
+    const DeckListPage(),
+    const Placeholder(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _pages[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        items: const [
+          BottomNavigationBarItem(
+            backgroundColor: Color.fromARGB(255, 129, 49, 76),
+            icon: Icon(Icons.question_mark),
+            label: 'Mistery Packet',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.credit_card),
+            label: 'Cards',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.dashboard),
+            label: 'Decks',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite),
+            label: 'Whishlist',
+          ),
+        ],
+      ),
+    );
+  }
 }
