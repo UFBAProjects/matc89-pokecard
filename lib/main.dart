ce8f5544bba230b79cc6def1afbcbd1a1257cf65
Núcleo de Desenvolvimento NDEAI <145492116+ndeai-projects@users.noreply.github.com>
2025-02-11 21:25:36 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -5,10 +5,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'package:pokecard/mistery_packet/mistery_packet_list_page.dart';
 
-import 'package:firebase_messaging/firebase_messaging.dart';
-import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
-final navigatorKey = GlobalKey<NavigatorState>();
 
 Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
   await Firebase.initializeApp();
